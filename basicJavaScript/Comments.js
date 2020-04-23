4c57919f2a947e1b48475e357053c7591d682920
dev-autodidacta <jdevunix@gmail.com>
2020-04-23 15:26:29 +0200

    Learning how to comment code in JS

---
+++ basicJavaScript/Comments.js
@@ -0,0 +1,18 @@
+/*
+ Something that I'm going to use a lot are comments, for explain the exercise or the resolution of it. YEAH, THIS IS A COMMENT!
+ */
+
+// Solution of Basic JavaScript: Comment Your JavaScript Code 
+
+
+
+// Single line comment
+
+/* Multi
+line
+comment
+*/
+
+
+
+// 1% Complete :D
