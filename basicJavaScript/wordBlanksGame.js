b1fce600a071b9cd168de3d78cb7da49f5fbf15e
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 01:00:36 +0200

    Word Blanks Game

---
+++ basicJavaScript/wordBlanksGame.js
@@ -0,0 +1,28 @@
+// * Solution of Basic JavaScript: Word Blanks
+
+
+// ! Read the long explanation
+
+
+
+// ? In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
+
+// ? ou will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks = myNoun + " " + myVerb + " " + myAdjective  + " " + myAdverb;
+
+
+
+
+
+
+
+
+
+
+// & 32% Complete Hurra! :D
