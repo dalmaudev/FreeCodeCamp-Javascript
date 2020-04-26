a999dca584c164cd2956be0c29a9a663460ebc59
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 01:32:26 +0200

    Manipulate Arrays With unshift()

---
+++ basicJavaScript/unshift.js
@@ -0,0 +1,19 @@
+// * Solution of Basic JavaScript: Manipulate Arrays With unshift()
+
+// ! .unshift() works exactly like .push(), but instead of adding the element at the end of the array, unshift() adds the element at the beginning of the array.
+
+
+
+// ? Add ["Paul",35] to the beginning of the myArray variable using unshift().
+
+
+myArray.unshift(["Paul",35]);
+
+
+
+
+
+
+
+
+// & 40% Complete Hurra! :D
