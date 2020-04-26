025539eeaee07578e1f994d8595a33c80cc78984
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 17:29:31 +0200

    ubtract One Number from Another with JavaScript

---
+++ basicJavaScript/subtraction.js
@@ -0,0 +1,19 @@
+// * Solution of Basic JavaScript: Subtract One Number from Another with JavaScript
+
+// ! We can also subtract one number from another. JavaScript uses the - symbol for subtraction.
+
+
+// ? Change the 0 so the difference is 12.
+
+
+var difference = 45 - 33;
+
+
+
+
+
+
+
+
+
+// & 7% Complete Hurra! :D
