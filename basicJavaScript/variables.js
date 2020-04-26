a0269d4b7b76abcf08dee20492e8cdb39b470375
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 16:48:57 +0200

    Declare JavaScript Variables

---
+++ basicJavaScript/variables.js
@@ -0,0 +1,19 @@
+// * Solution of Basic JavaScript: Declare JavaScript Variables
+
+// ! JavaScript provides seven different data types which are undefined, null, boolean, string, symbol, number, and object. Variables allow computers to store and manipulate data in a dynamic fashion. 
+
+// ! Use the var keyword to create a variable called myName.
+
+// ? Solution
+
+var myName;
+
+
+
+
+
+
+
+
+
+// & 2% Complete Hurra! :D
