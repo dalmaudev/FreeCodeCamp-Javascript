2dad45dc95dba5c3433beb047bdf044ecc3b5bd1
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 16:58:15 +0200

    Storing Values with the Assignment Operator

---
+++ basicJavaScript/assigment.js
@@ -0,0 +1,21 @@
+// * Solution of Basic JavaScript: Storing Values with the Assignment Operator
+
+// ! In JavaScript, you can store a value in a variable with the assignment operator. For example: myVariable = 5;
+
+// ! Assign the value 7 to variable a. Assign the contents of a to variable b.
+
+// ? Solution
+
+a = 7;
+b = 7;
+b = a;
+
+
+
+
+
+
+
+
+
+// & 3% Complete Hurra! :D
