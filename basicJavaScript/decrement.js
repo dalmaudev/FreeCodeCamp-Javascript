36830e79df69bfdb3452af6b7a144c3521acd7da
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 17:43:46 +0200

    Decrement a Number with JavaScript

---
+++ basicJavaScript/decrement.js
@@ -0,0 +1,19 @@
+// * Solution of Basic JavaScript: Decrement a Number with JavaScript
+
+// ! You can easily decrement or decrease a variable by one with the -- operator.
+
+
+// ? Change the code to use the -- operator on myVar.
+
+
+myVar--;
+
+
+
+
+
+
+
+
+
+// & 11% Complete Hurra! :D
