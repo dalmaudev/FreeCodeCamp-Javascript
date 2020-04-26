b07f18ef950ddac9697e8575f57941bf76b74f53
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 18:11:02 +0200

    Compound Assignment With Augmented Subtraction

---
+++ basicJavaScript/augmentedSubtraction.js
@@ -0,0 +1,21 @@
+// * Solution of Basic JavaScript: Compound Assignment With Augmented Subtraction
+
+// ! Like the += operator, -= subtracts a number from a variable.
+
+
+// ? Convert the assignments for a, b, and c to use the -= operator.
+
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+
+
+
+
+
+
+
+// & 16% Complete Hurra! :D
