d1f32183c66d600bf2d68e8f448d513368c39d3f
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 01:12:06 +0200

    Access Array Data with Indexes

---
+++ basicJavaScript/arrays2.js
@@ -0,0 +1,19 @@
+// * Solution of Basic JavaScript: Access Array Data with Indexes
+
+// ! We can access the data inside arrays using indexes. Array indexes are written in the same bracket notation that strings use, except that instead of specifying a character, they are specifying an entry in the array. 
+
+
+
+// ? Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+
+
+var myData = myArray[0];
+
+
+
+
+
+
+
+
+// & 35% Complete Hurra! :D
