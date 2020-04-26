a3a508e22de9d0fbb8cdfda82b7dc363557820af
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 18:05:33 +0200

    Compound Assignment With Augmented Addition

---
+++ basicJavaScript/compound.js
@@ -0,0 +1,21 @@
+// * Solution of Basic JavaScript: Compound Assignment With Augmented Addition
+
+// ! In programming, it is common to use assignments to modify the contents of a variable. 
+
+
+// ? Convert the assignments for a, b, and c to use the += operator.
+
+
+a += 12;
+b += 9;
+c += 7;
+
+
+
+
+
+
+
+
+
+// & 15% Complete Hurra! :D
