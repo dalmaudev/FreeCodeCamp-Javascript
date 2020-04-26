4adf4fa09ecd5ab4683841b109026bba1a7cd75e
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 18:02:14 +0200

    Finding a Remainder in JavaScript

---
+++ basicJavaScript/remainder.js
@@ -0,0 +1,19 @@
+// * Solution of Basic JavaScript: Finding a Remainder in JavaScript
+
+// ! The remainder operator % gives the remainder of the division of two numbers.
+
+
+// ? Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+
+
+var remainder = 17 % 3;
+
+
+
+
+
+
+
+
+
+// & 15% Complete Hurra! :D
