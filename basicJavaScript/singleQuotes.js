5bb3118c2d323306c1daf415ae2027ac74a5518b
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 18:52:33 +0200

    Quoting Strings with Single Quotes

---
+++ basicJavaScript/singleQuotes.js
@@ -0,0 +1,25 @@
+// * Solution of Basic JavaScript: Quoting Strings with Single Quotes
+
+
+// ! doubleQuoteStr = "This is a string"; 
+// ! singleQuoteStr = 'This is also a string';
+
+// ! goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"'; 
+// ! badStr = 'Finn responds, "Let's go!"'; // Throws an error
+
+// ? Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+
+// ? Right now, the <a> tag in the string uses double quotes everywhere. You will need to change the outer quotes to single quotes so you can remove the escape characters.
+
+
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+
+
+
+
+
+
+// & 21% Complete Hurra! :D
