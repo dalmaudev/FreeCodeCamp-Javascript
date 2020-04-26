8d6bf6d573992c19d1abaf8244416bec4655c637
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 18:36:07 +0200

    Escaping Literal Quotes in Strings

---
+++ literalQuotes.js
@@ -0,0 +1,24 @@
+// * Solution of Basic JavaScript: Escaping Literal Quotes in Strings
+
+
+// ! When you are defining a string you must start and end with a single or double quote. What happens when you need a literal quote: " or ' inside of your string?
+
+// ! var sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+
+
+// ? Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+
+// ? I am a "double quoted" string inside "double quotes".
+
+
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+
+
+
+
+
+
+
+// & 20% Complete Hurra! :D
