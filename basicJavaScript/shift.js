7e64a444da49e7d3a64c4020339fa569cb50741f
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 01:30:10 +0200

    Manipulate Arrays With shift()

---
+++ basicJavaScript/shift.js
@@ -0,0 +1,19 @@
+// * Solution of Basic JavaScript: Manipulate Arrays With shift()
+
+// ! .shift() works just like .pop(), except it removes the first element instead of the last.
+
+
+
+// ? Use the .shift() function to remove the first item from myArray, assigning the "shifted off" value to removedFromMyArray.
+
+
+var removedFromMyArray =  myArray.shift();
+
+
+
+
+
+
+
+
+// & 39% Complete Hurra! :D
