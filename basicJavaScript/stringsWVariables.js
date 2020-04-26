56d8195dc8f845526ca8b953a7add3628922365b
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 20:56:28 +0200

    Constructing Strings with Variables

---
+++ basicJavaScript/stringsWVariables.js
@@ -0,0 +1,25 @@
+// * Solution of Basic JavaScript: Constructing Strings with Variables
+
+
+// ! By using the concatenation operator (+), you can insert one or more variables into a string you're building.
+
+// ! var ourName = "freeCodeCamp";
+// ! var ourStr = "Hello, our name is " + ourName + ", how are you?";
+// ! ourStr is now "Hello, our name is freeCodeCamp, how are you?"
+
+
+
+// ? Set myName to a string equal to your name and build myStr with myName between the strings "My name is " and " and I am well!"
+
+
+
+var myName = "Jordi";
+var myStr = "My name is " + myName + "and I am well!";
+
+
+
+
+
+
+
+// & 25% Complete Hurra! :D
