494549bc9b29518e5772f021dd46fa12a392951c
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 18:12:53 +0200

    Compound Assignment With Augmented Multiplication

---
+++ basicJavaScript/augmentedMulti.js
@@ -0,0 +1,21 @@
+// * Solution of Basic JavaScript: Compound Assignment With Augmented Multiplication
+
+// ! The *= operator multiplies a variable by a number.
+
+
+// ? Convert the assignments for a, b, and c to use the *= operator.
+
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+
+
+
+
+
+
+
+// & 17% Complete Hurra! :D
