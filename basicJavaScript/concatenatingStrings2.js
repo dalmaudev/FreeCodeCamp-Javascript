39abcee641e0f77d74b0d7d808c8df575d89b7ad
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 19:14:19 +0200

    Concatenating Strings with Plus Equals Operator

---
+++ basicJavaScript/concatenatingStrings2.js
@@ -0,0 +1,24 @@
+// * Solution of Basic JavaScript: Concatenating Strings with the Plus Equals Operator
+
+
+// ! We can also use the += operator to concatenate a string onto the end of an existing string variable. This can be very helpful to break a long string over several lines.
+
+// ! var ourStr = "I come first. ";
+// ! ourStr += "I come second.";
+
+
+// ? Build myStr over several lines by concatenating these two strings: "This is the first sentence. " and "This is the second sentence." using the += operator. Use the += operator similar to how it is shown in the editor. Start by assigning the first string to myStr, then add on the second string.
+
+
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
+
+
+
+
+
+
+
+
+// & 24% Complete Hurra! :D
