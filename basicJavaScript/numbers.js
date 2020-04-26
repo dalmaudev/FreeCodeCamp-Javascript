e59d904a11462b04e9f90f56edd4483465c1a7ac
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 17:25:40 +0200

    Add Two Numbers with JavaScript

---
+++ basicJavaScript/numbers.js
@@ -0,0 +1,19 @@
+// * Solution of Basic JavaScript: Add Two Numbers with JavaScript
+
+// ! Number is a data type in JavaScript which represents numeric data.
+
+
+// ? Change the 0 so that sum will equal 20.
+
+
+var sum = 10 + 10;
+
+
+
+
+
+
+
+
+
+// & 6% Complete Hurra! :D
