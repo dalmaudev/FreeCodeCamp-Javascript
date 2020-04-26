4b168e94759a6fa63c7f8a768816bc65affae76a
dev-autodidacta <jdevunix@gmail.com>
2020-04-26 17:30:55 +0200

    Multiply Two Numbers with JavaScript

---
+++ basicJavaScript/multiply.js
@@ -0,0 +1,19 @@
+// * Solution of Basic JavaScript: Multiply Two Numbers with JavaScript
+
+// ! We can also multiply one number by another.JavaScript uses the * symbol for multiplication of two numbers.
+
+
+// ? Change the 0 so that product will equal 80.
+
+
+var product = 8 * 10;
+
+
+
+
+
+
+
+
+
+// & 8% Complete Hurra! :D
