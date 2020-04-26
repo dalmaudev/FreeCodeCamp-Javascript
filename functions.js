b087fb694d2fd6d9f7ed4cf93093d1bf811eb80c
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 01:40:01 +0200

    Write Reusable JavaScript with Functions

---
+++ functions.js
@@ -0,0 +1,26 @@
+// * Solution of Basic JavaScript: Write Reusable JavaScript with Functions
+
+
+// ! In JavaScript, we can divide up our code into reusable parts called functions. You can call or invoke this function by using its name followed by parentheses, like this: functionName();
+
+// ! var firstName = "Ada";
+// ! var secondLetterOfFirstName = firstName[1]; // secondLetterOfFirstName is "d"
+
+
+
+// ?  1. Create a function called reusableFunction which prints "Hi World" to the dev console. 2. Call the function.
+
+
+function reusableFunction() {
+    console.log("Hi World");
+  }
+  
+  reusableFunction();
+
+
+
+
+
+
+
+// & 42% Complete Hurra! :D
