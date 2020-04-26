178864d4bd07432410a2ba6f8f4c5f42154bcd0e
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 01:26:35 +0200

    Manipulate Arrays With pop()

---
+++ basicJavaScript/pop.js
@@ -0,0 +1,21 @@
+// * Solution of Basic JavaScript: Manipulate Arrays With pop()
+
+// ! Another way to change the data in an array is with the .pop() function.
+
+// ! .pop() removes the last element from an array and returns that element.
+
+
+
+// ? Use the .pop() function to remove the last item from myArray, assigning the "popped off" value to removedFromMyArray.
+
+
+var removedFromMyArray = myArray.pop();
+
+
+
+
+
+
+
+
+// & 38% Complete Hurra! :D
