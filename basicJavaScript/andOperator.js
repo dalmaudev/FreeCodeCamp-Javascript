8c6e0030d36d8c154819901efa144a4bc2e84d74
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 11:21:37 +0200

    Comparisons with the Logical And Operator

---
+++ basicJavaScript/andOperator.js
@@ -0,0 +1,25 @@
+// * Solution of Basic JavaScript: Comparisons with the Logical And Operator
+
+
+// ! Sometimes you will need to test more than one thing at a time. The logical and operator (&&) returns true if and only if the operands to the left and right of it are true.
+
+
+// ? Replace the two if statements with one statement, using the && operator, which will return "Yes" if val is less than or equal to 50 and greater than or equal to 25. Otherwise, will return "No".
+
+
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val <= 50 && val >= 25) {
+        return "Yes";
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);
+
+
+
+// & 60% Complete Hurra! :D
