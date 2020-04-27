f291196b85f5fed4696968830c0b9706d79a2901
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 10:38:51 +0200

    Comparison with the Equality Operator

---
+++ basicJavaScript/equalityOperator.js
@@ -0,0 +1,22 @@
+// * Solution of Basic JavaScript: Comparison with the Equality Operator
+
+
+// ! There are many comparison operators in JavaScript. All of these operators return a boolean true or false value.
+// ! The most basic operator is the equality operator ==. The equality operator compares two values and returns true if they're equivalent or false if they are not.
+
+// ? Add the equality operator to the indicated line so that the function will return "Equal" when val is equivalent to 12.
+
+
+// Setup
+function testEqual(val) {
+    if (val == 12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testEqual(10);
+
+
+
+// & 52% Complete Hurra! :D
