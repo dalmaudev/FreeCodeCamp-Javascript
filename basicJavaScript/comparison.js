9757ae8137602f4088d4387406b3b432da6b9b18
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 10:51:56 +0200

    Practice comparing different values

---
+++ basicJavaScript/comparison.js
@@ -0,0 +1,21 @@
+// * Solution of Basic JavaScript: Practice comparing different values
+
+
+// ! In the last two challenges, we learned about the equality operator (==) and the strict equality operator (===). Let's do a quick review and practice using these operators some more.
+
+// ? The compareEquality function in the editor compares two values using the equality operator. Modify the function so that it returns "Equal" only when the values are strictly equal.
+
+
+// Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+
+
+
+// & 54% Complete Hurra! :D
