5286959180e7b8f2864064145790fbb001ebc514
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 15:11:03 +0200

    omparisons with the Logical Or Operator

---
+++ basicJavaScript/orOperator.js
@@ -0,0 +1,25 @@
+// * Solution of Basic JavaScript: Comparisons with the Logical Or Operator
+
+
+// ! The logical or operator (||) returns true if either of the operands is true. Otherwise, it returns false. The logical or operator is composed of two pipe symbols: (||). This can typically be found between your Backspace and Enter keys.
+
+
+// ? Combine the two if statements into one statement which returns "Outside" if val is not between 10 and 20, inclusive. Otherwise, return "Inside".
+
+
+function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val < 10 || val > 20) {
+      return "Outside";
+    }
+  
+    // Only change code above this line
+    return "Inside";
+  }
+  
+  console.log(testLogicalOr(11));
+
+
+
+// & 61% Complete Hurra! :D
