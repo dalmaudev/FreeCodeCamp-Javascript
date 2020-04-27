ac99a693ae2d9cc1d214c26a1e9d641eb47aa4c7
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 21:32:13 +0200

    Introducing Else If Statements

---
+++ basicJavaScript/elseIf.js
@@ -0,0 +1,30 @@
+// * Solution of Basic JavaScript: Introducing Else If Statements
+
+
+// ! If you have multiple conditions that need to be addressed, you can chain if statements together with else if statements.
+
+
+// ? Convert the logic to use else if statements.
+
+
+function testElseIf(val) {
+
+    if (val > 10) {
+
+      return "Greater than 10";
+
+    }else if (val < 5) {
+
+      return "Smaller than 5";
+
+    }else{
+
+    return "Between 5 and 10";
+
+    }
+}
+
+testElseIf(7);
+
+
+// & 63% Complete Hurra! :D
