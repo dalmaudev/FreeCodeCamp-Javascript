8483ee5ebc576458cb7ae111fc85eb724dfc67b2
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 10:31:13 +0200

    Use Conditional Logic with If Statements

---
+++ basicJavaScript/conditionalIf.js
@@ -0,0 +1,23 @@
+// * Solution of Basic JavaScript: Use Conditional Logic with If Statements
+
+
+// ! If statements are used to make decisions in code. The keyword if tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses. These conditions are known as Boolean conditions and they may only be true or false.
+
+
+// ? Create an if statement inside the function to return "Yes, that was true" if the parameter wasThatTrue is true and return "No, that was false" otherwise.
+
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+  
+    // Only change code above this line
+  
+  }
+
+
+
+// & 51% Complete Hurra! :D
