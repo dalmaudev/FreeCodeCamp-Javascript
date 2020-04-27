a736ef136f2a206875c7d0a0783fe7d79938ac56
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 09:49:29 +0200

    Return a Value from a Function with Return

---
+++ basicJavaScript/return.js
@@ -0,0 +1,16 @@
+// * Solution of Basic JavaScript: Return a Value from a Function with Return
+
+
+// ! We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+
+
+// ? Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value. See the last line in the editor for an example of how you can test your timesFive function.
+
+
+function timesFive(a) {
+    return a * 5;
+}
+
+
+
+// & 46% Complete Hurra! :D
