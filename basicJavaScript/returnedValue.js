af911481e0916642f5ff8dbe2eb2610175a21b59
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 10:03:20 +0200

    Assignment with a Returned Value

---
+++ basicJavaScript/returnedValue.js
@@ -0,0 +1,25 @@
+// * Solution of Basic JavaScript: Assignment with a Returned Value
+
+
+// ! Assume we have pre-defined a function sum which adds two numbers together, then:
+
+// ! ourSum = sum(5, 12);
+
+// ! will call sum function, which returns a value of 17 and assigns it to ourSum variable.
+
+// ? Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+var processed = processArg(7);
+
+
+
+// & 48% Complete Hurra! :D
