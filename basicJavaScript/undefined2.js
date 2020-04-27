e55d947df482eea519b8a729f5d0972ad6841c50
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 09:54:53 +0200

    Undefined Value returned from a Function

---
+++ basicJavaScript/undefined2.js
@@ -0,0 +1,16 @@
+// * Solution of Basic JavaScript: Understanding Undefined Value returned from a Function
+
+
+// ! A function can include the return statement but it does not have to. In the case that the function doesn't have a return statement, when you call it, the function processes the inner code but the returned value is undefined.
+
+
+// ? Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+
+
+function addFive() {
+    sum = sum + 5;
+}
+// 
+
+
+// & 47% Complete Hurra! :D
