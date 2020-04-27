6e57b2fe00777923fe7b64c81f741ae0c25ac87b
dev-autodidacta <jdevunix@gmail.com>
2020-04-27 09:28:41 +0200

    Global Scope and Functions

---
+++ basicJavaScript/globalScope.js
@@ -0,0 +1,24 @@
+// * Solution of Basic JavaScript: Global Scope and Functions
+
+
+// ! In JavaScript, scope refers to the visibility of variables. Variables which are defined outside of a function block have Global scope. This means, they can be seen everywhere in your JavaScript code.
+
+
+
+// ? Using var, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
+
+// ? Inside function fun1, assign 5 to oopsGlobal without using the var keyword.
+
+
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+}
+
+
+
+
+
+// & 44% Complete Hurra! :D
