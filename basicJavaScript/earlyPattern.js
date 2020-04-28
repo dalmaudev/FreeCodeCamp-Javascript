72fd38d676148c0ca964dd362ff8e673760e674e
dev-autodidacta <jdevunix@gmail.com>
2020-04-28 15:04:49 +0200

    Return Early Pattern for Functions

--- basicJavaScript/earlyPattern.js
+++ basicJavaScript/earlyPattern.js
@@ -1,6 +1,7 @@
 // * Solution of Basic JavaScript: Return Early Pattern for Functions
 
 
+
 // ! When a return statement is reached, the execution of the current function stops and control returns to the calling location.
 
 
