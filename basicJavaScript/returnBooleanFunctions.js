42adf0b5e876c3080e4523968540c25d6c981959
dev-autodidacta <jdevunix@gmail.com>
2020-04-28 14:26:22 +0200

    Returning Boolean Values from Functions

---
+++ basicJavaScript/returnBooleanFunctions.js
@@ -0,0 +1,20 @@
+// * Solution of Basic JavaScript: Returning Boolean Values from Functions
+
+
+// ! You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+
+
+// ? Fix the function isLess to remove the if/else statements.
+
+
+function isLess(a, b) {
+    // Only change code below this line
+    return a <= b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
+
+
+
+// & 70% Complete Hurra! :D
