866c2a119bebc2d9d955fb8d3ec0ade9ed78579e
dev-autodidacta <jdevunix@gmail.com>
2020-04-29 23:36:09 +0200

    Add New Properties to a JavaScript Object

---
+++ basicJavaScript/addNewPropertyObjects.js
@@ -0,0 +1,26 @@
+// * Solution of Basic JavaScript: Add New Properties to a JavaScript Object
+
+// ! You can add new properties to existing JavaScript objects the same way you would modify them. Here's how we would add a "bark" property to ourDog: ourDog.bark = "bow-wow";
+
+
+// ? Add a "bark" property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
+
+
+// Setup
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  // Only change code below this line
+  
+  myDog.bark = "woof";
+
+
+
+
+
+
+// & 77% Complete Hurra! :D
