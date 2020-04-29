2becfae87d197e6459cefaa60dc5f41a7bfda188
dev-autodidacta <jdevunix@gmail.com>
2020-04-29 17:51:18 +0200

    Build JavaScript Objects

---
+++ basicJavaScript/objects.js
@@ -0,0 +1,30 @@
+// * Solution of Basic JavaScript: Build JavaScript Objects
+
+// ! Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+// ! Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+
+// ? Make an object that represents a dog called myDog which contains the properties "name" (a string), "legs", "tails" and "friends".
+
+// ? You can set these object properties to whatever values you want, as long "name" is a string, "legs" and "tails" are numbers, and "friends" is an array.
+
+
+var myDog = {
+    // Only change code below this line
+    
+    "name": "Jordi",
+    "legs": 2,
+    "tails": 1,
+    "friends": ["Dog1", "Dog2"]
+    
+    // Only change code above this line
+    };
+
+
+
+
+
+
+
+
+// & 33% Complete Hurra! :D
