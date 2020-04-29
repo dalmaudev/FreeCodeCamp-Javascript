6930bc58a87366d0c1c8f329b418b1da243d8f2c
dev-autodidacta <jdevunix@gmail.com>
2020-04-29 18:47:51 +0200

    Accessing Object Properties with Variables

---
+++ basicJavaScript/accessingObjectsVariables.js
@@ -0,0 +1,26 @@
+// * Solution of Basic JavaScript: Accessing Object Properties with Variables
+
+// ! Another use of bracket notation on objects is to access a property which is stored as the value of a variable. This can be very useful for iterating through an object's properties or when accessing a lookup table.
+
+
+// ? Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
+
+
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  
+  var playerNumber = 16;       // Change this line
+  var player = testObj[playerNumber];   // Change this line
+
+
+
+
+
+
+// & 75% Complete Hurra! :D
