26673e9e8f00155b5a063a1539c40e22aeabbd47
dev-autodidacta <jdevunix@gmail.com>
2020-05-03 16:43:45 +0200

    Manipulating Complex Objects

---
+++ basicJavaScript/complexObjects.js
@@ -0,0 +1,37 @@
+// * Solution of Basic JavaScript: Manipulating Complex Objects
+
+
+// ! Sometimes you may want to store data in a flexible Data Structure. A JavaScript object is one way to handle flexible data. They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects. View FCC example.
+
+// ? Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+
+
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Eminem",
+      "title": "7 Miles",
+      "release_year": 2020,
+      "formats": [
+        "MKV",
+        "LP"
+      ]
+    }
+    // Add a record here
+  ];
+
+
+
+
+// & 81% Complete Hurra! :D
