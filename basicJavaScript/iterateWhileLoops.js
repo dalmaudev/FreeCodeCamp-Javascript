304e285269492106ede0fd66bddc262c3604f99b
dev-autodidacta <jdevunix@gmail.com>
2020-05-04 12:35:59 +0200

    Iterate with JavaScript While Loops

---
+++ basicJavaScript/iterateWhileLoops.js
@@ -0,0 +1,22 @@
+// * Solution of Basic JavaScript: Iterate with JavaScript While Loops
+
+
+// ! You can run the same code multiple times by using a loop. The first type of loop we will learn is called a while loop because it runs "while" a specified condition is true and stops once that condition is no longer true. Check FCC example.
+
+// ? Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+var i = 5;
+while (i > -1) {
+    myArray.push(i);
+    i--;
+}
+
+
+
+
+// & 85% Complete Hurra! :D
