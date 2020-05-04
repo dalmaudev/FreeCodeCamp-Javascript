80f78441831394e02bf077ada3ac24e4b50a7f8f
dev-autodidacta <jdevunix@gmail.com>
2020-05-04 12:53:31 +0200

    Iterate with JavaScript For Loops

---
+++ basicJavaScript/iterateForLoops.js
@@ -0,0 +1,20 @@
+// * Solution of Basic JavaScript: Iterate with JavaScript For Loops
+
+
+// ! The most common type of JavaScript loop is called a for loop because it runs "for" a specific number of times. for ([initialization]; [condition]; [final-expression]) Check FCC explication
+
+// ? Use a for loop to work to push the values 1 through 5 onto myArray.
+
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for(var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
+
+
+
+
+// & 85% Complete Hurra! :D
