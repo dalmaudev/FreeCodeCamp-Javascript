a60eb57df6689e15231521823cad46eb4ca639dd
dev-autodidacta <jdevunix@gmail.com>
2020-05-04 12:57:24 +0200

    Iterate Odd Numbers With a For Loop

---
+++ basicJavaScript/iterateForLoops2.js
@@ -0,0 +1,20 @@
+// * Solution of Basic JavaScript: Iterate Odd Numbers With a For Loop
+
+
+// ! For loops don't have to iterate one at a time. By changing our final-expression, we can count by even numbers. We'll start at i = 0 and loop while i < 10. We'll increment i by 2 each loop with i += 2.
+
+// ? Push the odd numbers from 1 through 9 to myArray using a for loop.
+
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for(var i = 1; i < 10; i +=2){
+    myArray.push(i);
+}
+
+
+
+
+// & 86% Complete Hurra! :D
