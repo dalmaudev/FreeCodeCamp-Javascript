8a59229bd8d21dd1f0c350edbf5e53b4ad94480d
dev-autodidacta <jdevunix@gmail.com>
2020-05-04 13:06:56 +0200

    Count Backwards With a For Loop

---
+++ basicJavaScript/countBackForLoop.js
@@ -0,0 +1,20 @@
+// * Solution of Basic JavaScript: Count Backwards With a For Loop
+
+
+// ! A common task in JavaScript is to iterate through the contents of an array. One way to do that is with a for loop. Check FCC example
+
+// ? Push the odd numbers from 1 through 9 to myArray using a for loop.
+
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for(var i = 9; i > 0; i -=2){
+    myArray.push(i);
+}
+
+
+
+
+// & 87% Complete Hurra! :D
