7542d3f73a8c90b1120d8747ed521ec9cb63b2b7
dev-autodidacta <jdevunix@gmail.com>
2020-05-04 13:59:02 +0200

    Iterate Through an Array with a For Loop

---
+++ basicJavaScript/iterateArrayForLoop.js
@@ -0,0 +1,21 @@
+// * Solution of Basic JavaScript: Iterate Through an Array with a For Loop
+
+
+// ! A common task in JavaScript is to iterate through the contents of an array. One way to do that is with a for loop. Check FCC example
+
+// ? Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
+
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+for (var i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+
+
+
+
+// & 88% Complete Hurra! :D
