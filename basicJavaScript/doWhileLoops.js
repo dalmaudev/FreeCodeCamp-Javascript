169ab4681f4c738db9252234de676ac32f2bbec6
dev-autodidacta <jdevunix@gmail.com>
2020-05-07 09:51:32 +0200

    Iterate with JavaScript Do...While Loops

---
+++ basicJavaScript/doWhileLoops.js
@@ -0,0 +1,27 @@
+// * Solution of Basic JavaScript: Iterate with JavaScript Do...While Loops
+
+
+// ! The next type of loop you will learn is called a do...while loop. It is called a do...while loop because it will first do one pass of the code inside the loop no matter what, and then continue to run the loop while the specified condition evaluates to true. Check all FCC example
+
+
+// ? Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
+
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line
+do {
+    myArray.push(i);
+    i++;
+}
+while (i < 5) {
+  
+}
+  
+  
+
+
+
+// & 90% Complete Hurra! :D
